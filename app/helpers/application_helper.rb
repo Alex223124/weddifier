dd6babba9ca0d62da3ecf569743343f4af611701
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-08 11:28:56 -0600

    Add invite and remove links to guests on admin panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def guest_invite_status(guest)
+    if guest.invited?
+      'Invited'
+    else
+      link_to 'Invite', guest_invitations_path(guest), method: :post
+    end
+  end
 end
