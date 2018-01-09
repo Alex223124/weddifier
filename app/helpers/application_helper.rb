b5ba5f2a81d42cd7432f5f88ab84e2fccc0a4247
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-09 12:08:08 -0600

    Move application helpers to admin index helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  def guest_invite_status(guest)
-    if guest.invited?
-      'Invited'
-    else
-      link_to 'Invite', guest_invitations_path(guest), method: :post,
-        remote: true
-    end
-  end
-
-  def guest_remove_link(guest)
-    if guest.invited?
-      ''
-    else
-      link_to 'Remove', guest_path(guest), method: :delete, data:
-        { confirm: 'Are you sure?' }, remote: true
-    end
-  end
 end
