7f496204163b5ff4dd6c536d4f93e3fd782de694
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-10 15:56:16 -0600

    Add admin class to body on search results page too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_admin_path?
     path = request.env['PATH_INFO']
-    !!(path == '/admin' || path == '/admin/login')
+    !!(path == '/admin' || path == '/admin/login' || path == '/admin/search')
   end
 end
