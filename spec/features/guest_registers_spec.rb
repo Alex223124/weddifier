3a15f6cf3e5ba99468d6ca7e275da3516d7f7266
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-11 14:26:22 -0600

    Refactor user registration integration spec

--- spec/features/guest_registers_spec.rb
+++ spec/features/guest_registers_spec.rb
@@ -4,13 +4,7 @@ feature 'User registration' do
   scenario 'User visits root path and registers successfully' do
     expect(Guest.count).to eq(0)
     visit root_path
-    fill_in 'First name' , with: 'John'
-    fill_in 'Last name' , with: 'Doe'
-    fill_in 'Father surname' , with: 'Mckenzie'
-    fill_in 'Mother surname' , with: 'Sullivan'
-    fill_in 'Phone' , with: '1234567890'
-    fill_in 'Email' , with: 'example@email.com'
-    click_on 'Register'
+    fill_form_correctly
     expect(page).to have_current_path(thank_you_path)
     expect(Guest.count).to eq(1)
   end
@@ -18,11 +12,25 @@ feature 'User registration' do
   scenario 'User vists root path and registers unsuccessfully' do
     expect(Guest.count).to eq(0)
     visit root_path
-    fill_in 'First name' , with: 'John'
-    click_on 'Register'
+    fill_form_incorrectly
     expect(page).to have_current_path(guests_path)
     expect(page).to have_content('There are some errors in your form.')
     expect(page).to have_content('Phone is too short')
     expect(Guest.count).to eq(0)
+  end
+
+  def fill_form_correctly
+    fill_in 'First name' , with: 'John'
+    fill_in 'Last name' , with: 'Doe'
+    fill_in 'Father surname' , with: 'Mckenzie'
+    fill_in 'Mother surname' , with: 'Sullivan'
+    fill_in 'Phone' , with: '1234567890'
+    fill_in 'Email' , with: 'example@email.com'
+    click_on 'Register'
+  end
+
+  def fill_form_incorrectly
+    fill_in 'First name' , with: 'John'
+    click_on 'Register'
   end
 end
