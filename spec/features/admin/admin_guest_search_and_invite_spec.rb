bbe25ee98eadbc5f569c1f8b9ee80a9c9ea5131a
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-12 10:12:30 -0600

    Add admin search feature specs

---
+++ spec/features/admin/admin_guest_search_and_invite_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+require 'capybara/poltergeist'
+
+feature 'Admin search' do
+  let!(:john) { Fabricate(:guest, first_name: 'John') }
+  let!(:dave) { Fabricate(:guest, first_name: 'Dave') }
+
+  background do
+    # Needs windows running chromedriver
+    # This sets Capybara up to use a REMOTE Selenium server
+    # Capybara.javascript_driver = :selenium_remote_chrome
+    # Capybara.register_driver "selenium_remote_chrome".to_sym do |app|
+     # Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://localhost:9515", desired_capabilities: :chrome)
+    # end
+
+    # Does not support ES6
+    # Capybara.javascript_driver = :poltergeist
+    # Capybara.register_driver :poltergeist do |app|
+    #   Capybara::Poltergeist::Driver.new(app, window_size: [2000, 2000])
+    # end
+
+    # Works but get a weird RSpec formatting error
+    Capybara.javascript_driver = :selenium_chrome_headless
+
+    # Can not connect
+    # Capybara.javascript_driver = :webkit
+
+    # Capybara.register_driver :chrome do |app|
+    #   Capybara::Selenium::Driver.new app, browser: :chrome,
+    #     options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+    # end
+
+    # Capybara.javascript_driver = :chrome
+
+    admin_browser_login
+  end
+
+  scenario 'Admin searches for a guest' do
+    expect(page).to have_content('John')
+    expect(page).to have_content('Dave')
+    expect(page).to have_content(
+      'Displaying  guests: Total: (2) - Invited: (0) - Remaining: (2)')
+
+    search_for('john')
+
+    expect(page).to have_content('John')
+    expect(page).not_to have_content('Dave')
+    expect(page).to have_content(
+      'Displaying  guests: Total: (1) - Invited: (0) - Remaining: (1)')
+  end
+
+  scenario 'Admin searches for a guest and single invites from search results', js: true do
+    expect_invite_button_to_be_present
+
+    search_for('john')
+    click_on('Invite')
+
+    expect(page).to have_content('Successfully invited guest John')
+    expect_invite_button_not_to_be_present
+  end
+
+  scenario 'Admin searches for a guest and bulk invites from search results', js: true do
+    expect_invite_button_to_be_present
+
+    search_for('john')
+    find(:css, "#select_all").set true
+    find('input[value="Invite Selected"]', match: :first).click
+
+    expect(page).to have_content('Guests invited successfully.')
+    expect_invite_button_not_to_be_present
+    expect(page).to have_content(format_date(john.invitation.created_at))
+  end
+
+  def expect_invite_button_to_be_present
+    expect(page).to have_selector('a', text: /\bInvite\b/)
+    expect(page).not_to have_selector('input[value="Invited"]')
+  end
+
+  def expect_invite_button_not_to_be_present
+    expect(page).to have_selector('input[value="Invited"]')
+    expect(page).not_to have_selector('a', text: /\bInvite\b/)
+  end
+
+  def format_date(date)
+    date.in_time_zone('America/Mexico_City').strftime("%A %d %B %l:%M%P")
+  end
+
+  def search_for(guest_name)
+    fill_in 'Search', with: guest_name
+    click_on 'Search'
+  end
+end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -2,3 +2,14 @@ def admin_login
   admin = Fabricate(:admin)
   post admin_login_path, params: { email: admin.email, password: admin.password }
 end
+
+def admin_browser_login
+  admin = Fabricate(:admin)
+  visit admin_login_path
+
+  fill_in 'Email', with: admin.email
+  fill_in 'Password', with: admin.password
+  click_on 'Login'
+
+  expect(page).to have_current_path(admin_path)
+end
