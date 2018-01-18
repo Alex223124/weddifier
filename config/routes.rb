3b0d593056c90325694910d92284dd00e1a9661d
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2018-01-17 21:28:05 -0600

    Remove unnecessary home_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'guests#new'
   get '/thanks', to: 'pages#thanks', as: 'thank_you'
-  get '/home', to: 'pages#home'
 
   resources :guests, only: [:new, :create, :destroy] do
     resources :invitations, only: [:create]
